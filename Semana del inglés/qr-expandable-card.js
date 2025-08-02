3a261a440c6df743aa820ff0c43965de8d1024e6
K3I5Y <keisy.moreno@cebjda.com>
2025-08-01 20:42:34 -0500

    Delete Semana del inglés directory

--- Semana del inglés/qr-expandable-card.js
+++
@@ -1,8 +0,0 @@
-function expandQR(imageSrc) {
-  document.getElementById('qrExpanded').src = imageSrc;
-  document.getElementById('qrModal').style.display = 'flex';
-}
-
-function closeQR() {
-  document.getElementById('qrModal').style.display = 'none';
-}
